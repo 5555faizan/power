223172fd40728710ff175c13ad7d013d99b662af
5555faizan <faizannawaz3085@gmail.com>
2024-08-13 20:38:57 +0500

    digital

---
+++ src/app/about/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function page() {
+  return (
+    <div>
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nesciunt voluptates expedita voluptas maxime cum, error ipsum quod veritatis harum ut debitis magnam unde repellendus a sit, nam tempore id voluptatibus!
+    </div>
+  )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,33 @@
+import React from 'react'
 
-<figure className="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800">
-  <img className="w-50 h-50 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto" src="/faizan.jpeg" alt="" width="512" height="800"/>
-  <div className="pt-6 md:p-8 text-center md:text-left space-y-4">
-    <blockquote>
-      <p className="text-lg font-medium">
-      We will help you to grow your business through social media marketing. Enjoy a digital media presence like never before!
-      </p>
-    </blockquote>
-    <figcaption className="font-medium">
-      <div className="text-sky-500 dark:text-sky-400">
-        Faizan Nawaz
-      </div>
-      <div className="text-slate-700 dark:text-slate-500">
-        social Media Marketer, Dynamic Bit
-      </div>
-    </figcaption>
-  </div>
-</figure>
+const page = () => {
+  return (
+    <>
+       <figure className="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800">
+        <img
+          className="w-50 h-50 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto"
+          src="/faizan.jpeg"
+          alt=""  
+          width="512"
+          height="800"
+        />
+        <div className="pt-6 md:p-8 text-center md:text-left space-y-4">
+          <blockquote>
+            <p className="text-lg font-medium">
+              We will help you to grow your business through social media
+              marketing. Enjoy a digital media presence like never before!
+            </p>
+          </blockquote>
+          <figcaption className="font-medium">
+            <div className="text-sky-500 dark:text-sky-400">Faizan Nawaz</div>
+            <div className="text-slate-700 dark:text-slate-500">
+              social Media Marketer, It Work Center
+            </div>
+          </figcaption>
+        </div>
+      </figure>
+    </>
+  )
+}
+
+export default page
